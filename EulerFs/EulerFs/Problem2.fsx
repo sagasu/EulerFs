f03139a8392142004576d4066658302fa8571724
Mateusz Kopij <sagasu@gmail.com>
2012-10-03 00:09:51 +0100

    Solved problem2 and problem3

---
+++ EulerFs/EulerFs/Problem2.fsx
@@ -0,0 +1,9 @@
+﻿#light
+
+let sumEvenFib (maxIter:int) =
+    let rec sumFib (prev:int) (curr:int) (sum:int) = 
+        match curr with
+        | a when (a < maxIter) && (a % 2 = 1) -> sumFib curr (prev + curr) (sum + curr)
+        | a when (a < maxIter) && (a % 2 = 0) -> sumFib curr (prev + curr) sum
+        | _ -> sum
+    sumFib 1 2 2 // The 2 in sum is a sum of first two even elements in fibonacci sequence
---
+++ EulerFs/EulerFs/Problem3.fsx
@@ -0,0 +1,32 @@
+﻿#light
+open System 
+
+// Not fast enought solution
+let largestPrimeFactor (number:decimal) =
+    let rec getLargestFactor (number:decimal) (startNumber:decimal) = 
+        match number with
+        | a when (a % startNumber = 0M) -> startNumber
+        | _ -> getLargestFactor number (startNumber - 1M)
+    let isPrime (n:decimal) = 
+        match n with
+        | a when a = 1M || a = 2M -> true
+        | _ -> match getLargestFactor n (n / 2M) with
+                | 1M -> true
+                | _ -> false
+    let rec getLargestPrimeFactor startNumber =
+        let factor = getLargestFactor number startNumber 
+        match factor with
+        | a when isPrime factor -> factor
+        | _ -> getLargestPrimeFactor (factor - 1M)
+    getLargestPrimeFactor( number / 2M)
+
+// Good solution
+let rec maxPrimeFactor (number:decimal) (divider:decimal) = 
+    if divider * divider > number then
+        number
+    elif number % divider = 0M then
+        maxPrimeFactor (number / divider) divider
+    else
+        maxPrimeFactor number (divider + 1M)
+        
+maxPrimeFactor 600851475143M 2M
