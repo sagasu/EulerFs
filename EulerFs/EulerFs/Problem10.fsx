78813ff51e8dadc0a0c4adad9b6ec1e9cf33ef1b
Mateusz Kopij <sagasu@gmail.com>
2012-10-05 22:38:06 +0100

    Problems 9 and 10 solved

---
+++ EulerFs/EulerFs/Problem10.fsx
@@ -0,0 +1,20 @@
+﻿#light
+
+let isOddPrime n =
+    let rec isOddPrime divider =
+        match divider with
+        | 1 -> true
+        | a when n % divider = 0 -> false
+        | _ -> isOddPrime (divider - 1)
+    isOddPrime (int (System.Math.Sqrt(float n)))
+
+let sumPrimes =
+    let rec sumPrimes (n:decimal) (sum:decimal) =
+        match n with
+        | a when a >= 2000000M -> sum //2000000
+        | _ -> 
+            if isOddPrime (int n) then
+                sumPrimes (n + 2M) (sum + n)
+            else
+                sumPrimes (n + 2M) sum
+    sumPrimes 3M 2M
---
+++ EulerFs/EulerFs/Problem9.fsx
@@ -0,0 +1,22 @@
+﻿#light
+// a * a + b * b = (1000 - a - b) * (1000 - a - b)
+
+let findPythagorean =
+    let rec findPythagorean2 a b =
+        match b with
+        | 1000 -> None
+        | _ -> 
+            if a * a + b * b = (1000 - a - b) * (1000 - a - b) then
+                Some (a, b, 1000 - a - b)
+            else
+                findPythagorean2 a (b + 1)
+    let rec findPythagorean a =
+        match a with
+        | 1000 -> None
+        | _ -> 
+            let pythagorean = findPythagorean2 a 1
+            if pythagorean = None then
+                findPythagorean (a + 1)
+            else
+                pythagorean
+    findPythagorean 1
