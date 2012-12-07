552794d9f314f6307b8cf885553752d5b6f3152d
Mateusz Kopij <sagasu@gmail.com>
2012-12-07 17:55:55 +0000

    working on a problem 22

--- EulerFs/EulerFs/Problem22.fsx
+++ EulerFs/EulerFs/Problem22.fsx
@@ -1,4 +1,13 @@
-﻿let problem22 = 
-    using(new System.IO.StreamReader "names.txt") (fun r -> 
+﻿let calculateValue (n:string) =
+    Seq.fold (fun (acc:bigint) (x:char) -> (acc+ (bigint(((int)x)-64)))) (bigint 0) n
+
+
+
+let problem22 = 
+    using(new System.IO.StreamReader @"C:\Users\Mateusz.Kopij\Documents\GitHub\EulerFs\EulerFs\EulerFs\names.txt") (fun r -> 
         let line = r.ReadToEnd()
-        line)
+        let names = line.Split(',')
+        let trimmedNames = Seq.map (fun (x:string) -> x.Trim([|'"'|])) names
+        let sortedNames = Seq.sort trimmedNames
+        Seq.fold (fun (sum:bigint, pos:bigint) x -> ((calculateValue x)*pos, (pos + (bigint 1)))) ((bigint 0), (bigint 1)) sortedNames
+        )
